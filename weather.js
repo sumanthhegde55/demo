650724dec595184cd42b4e8fa15bb5715c5d7e25
Sumanth Hegde <sumanthhegde55@gmail.com>
2021-01-13 13:05:01 +0530

    added invalid city names

--- weather.js
+++ weather.js
@@ -10,6 +10,7 @@ app.post("/",function(req,res){
   var city=req.body.city;
   const url="https://api.openweathermap.org/data/2.5/weather?q=" +city+"&appid=d9613a15f35a5d1cd0c5ca77204a5205&units=metric";
   https.get(url,function(res1){
+    if(res1.statusCode === 200){
     res1.on("data",function(data){
       const wdata=JSON.parse(data);
       const temp=wdata.main.temp;
@@ -20,11 +21,18 @@ app.post("/",function(req,res){
       res.write("<p> The weather is currently " + desc + "</p>");
       res.write("<img src="+imageURL+">");
       // res.redirect(__dirname+ "index.html");
-      res.write('<form action="/redirect" method="POST"><button type="submit" name="button">Try Another!</button></form>');
+      res.write('<form action="/redirect" method="POST"><button type="submit" name="button" autofocus>Try Another!</button></form>');
         res.send();
       // res.redirect(__dirname + "/redirect.html");
       // res.sendFile(__dirname + "/redirect.html");
-    });
+      });
+    }
+    else{
+        res.write('<script>alert("The name of the city entered is invalid! Try Again!")</script>')
+        res.write('<h1>WELCOME TO WEATHERAPP!</h1><h2>We provide weather data of any city u like!</h2>')
+        res.write('<form action="/redirect" method="POST"><button type="submit" name="button" autofocus>Try Another!</button></form>');
+          res.send();
+    }
   });
 });
 app.post("/redirect",function(req,res){
